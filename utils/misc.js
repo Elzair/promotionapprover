b6b5fbd32df2c0121df810d7346ae248f18ce09d
Elzair <elzairthesorcerer@gmail.com>
2013-07-10 16:09:38 -0400

    Added contains() method to utils to check if an array contains a certain value

--- utils/misc.js
+++ utils/misc.js
@@ -1,44 +1,63 @@
+exports.contains = function(arr, obj){
+  for (i in arr){
+    if (arr.hasOwnProperty(i) && arr[i] === obj){
+      return true;
+    }
+  }
+  return false;
+};
+
 exports.getProperty = function(obj, p){
-	if (obj == null || p == null)
-	  return null;
+  if (obj === null || p === null){
+    return null;
+  }
   return obj.hasOwnProperty(p) ? obj[p] : '';
-}
+};
 
 exports.getAllProperties = function(obj){
-  if (obj == null)
+  if (obj === null){
     return null;
-  var no = new Object();
-  for (prop in obj)
-    if (obj.hasOwnProperty(prop))
+  }
+  var no = {};
+  for (prop in obj){
+    if (obj.hasOwnProperty(prop)){
       no[prop] = obj[prop];
+    }
+  }
   return no;
-}
+};
 
 exports.sortProperties = function(obj){
-	if (obj == null)
-	  return obj;
-  var keys = [], nd = new Object();
-  for (var key in obj)
-    if (obj.hasOwnProperty(key))
+  if (obj === null){
+    return obj;
+  }
+  var keys = [], nd = {};
+  for (var key in obj){
+    if (obj.hasOwnProperty(key)){
       keys.push(key);
+    }
+  }
   keys = keys.sort();
   for (var k in keys)
     if (obj.hasOwnProperty(keys[k]))
       nd[keys[k]] = obj[keys[k]];
   return nd;
-}
+};
 
 exports.deleteProperty = function(obj, p){
-	if (obj == null)
-	  return obj;
-  if (obj.hasOwnProperty(p))
+  if (obj == null){
+    return obj;
+  }
+  if (obj.hasOwnProperty(p)){
     delete obj[p];
+  }
   return obj;
-}
+};
 
 exports.replaceQuotes = function(str){
-	if (str == null || str == '')
-	  return str;
+  if (str == null || str == ''){
+    return str;
+  }
   var newparams = [];
   var params = str.split(',');
   for (var param in params){
@@ -47,4 +66,4 @@ exports.replaceQuotes = function(str){
     newparams.push(kv.join(':'));
   }
   return newparams.join(',');
-}
+};
