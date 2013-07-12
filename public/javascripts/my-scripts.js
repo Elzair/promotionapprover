9ec46693e2775cf9d719dbd36856f86d12e55139
Elzair <elzairthesorcerer@gmail.com>
2013-07-12 13:52:13 -0400

    Neatened up two client-side javascript files

--- public/javascripts/my-scripts.js
+++ public/javascripts/my-scripts.js
@@ -1,44 +1,49 @@
-var utils = function(){
-	var sortProperties = function(dict){
-	  var keys = [];
-	  var nd = {};
-	  for (var key in dict)
-	    if (dict.hasOwnProperty(key))
-	      keys.push(key);
-	  keys = keys.sort();
-	  for (var k in keys)
-	    if (dict.hasOwnProperty(k))
-	      nd.push(dict[k]);
-	  return nd;
-	},
+var utils = function(Uri, Crypto){
+  var sortProperties = function(dict){
+    var keys = [];
+    var nd = {};
+    for (var key in dict){
+      if (dict.hasOwnProperty(key)){
+        keys.push(key);
+      }
+    }
+    keys = keys.sort();
+    for (var k in keys){
+      if (dict.hasOwnProperty(k)){
+        nd.push(dict[k]);
+      }
+    }
+    return nd;
+  },
 
-	deleteProperty = function(dict, el){
-	  delete dict[el];
-	  dict.length -= 1;
-	  return dict;
-	},
+  deleteProperty = function(dict, el){
+    delete dict[el];
+    dict.length -= 1;
+    return dict;
+  },
 
-	getProperty = function(dict, attr){
-	  return dict.hasOwnProperty(attr) ? dict[attr] : '';
-	},
+  getProperty = function(dict, attr){
+    return dict.hasOwnProperty(attr) ? dict[attr] : '';
+  },
 
-	completeUrl = function(url, hash){
-	  if (url == null || url == '' || hash == null || hash == '')
-	    return '';
-	  return url.toString();
-	  uri = new Uri(url).deleteQueryParam('timeStamp', 'signature').addQueryParam('timeStamp', new Date.UTC().toString());
-	  uri.setQueries(sortProperties(uri.getQueries());
-	  return uri.addQueryParam('signature', CryptoJS.HmacSHA512(uri.toString(), hash).toString()).toString();
-	},
-	
-	testMe = function(){
-		return 'This works!';
-	};
-	
-	return{
-		completeUrl: completeUrl,
-		deleteProperty: deleteProperty,
-		getProperty: getProperty,
-		sortProperties: sortProperties
-	};
+  completeUrl = function(url, hash){
+    if (url === null || url === '' || hash === null || hash === ''){
+      return '';
+    }
+    //return url.toString();
+    uri = new Uri(url).deleteQueryParam('timeStamp', 'signature').addQueryParam('timeStamp', new Date.UTC().toString());
+    uri.setQueries(sortProperties(uri.getQueries());
+    return uri.addQueryParam('signature', Crypto.HmacSHA512(uri.toString(), hash).toString()).toString();
+  },
+  
+  testMe = function(){
+    return 'This works!';
+  };
+  
+  return{
+    completeUrl: completeUrl,
+    deleteProperty: deleteProperty,
+    getProperty: getProperty,
+    sortProperties: sortProperties
+  };
 };
--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -1,27 +1,27 @@
 /*! MyUtils */
 function sortProperties(obj){
-	var keys = [], nd = new Object();
-	for (var key in obj)
-	  if (obj.hasOwnProperty(key))
-	    keys.push(key);
-	keys = keys.sort();
-	for (var k in keys)
-	  if (obj.hasOwnProperty(keys[k]))
-	    nd[keys[k]] = obj[keys[k]];
-	return nd;
+  var keys = [], nd = {};
+  for (var key in obj)
+    if (obj.hasOwnProperty(key))
+      keys.push(key);
+  keys = keys.sort();
+  for (var k in keys)
+    if (obj.hasOwnProperty(keys[k]))
+      nd[keys[k]] = obj[keys[k]];
+  return nd;
 }
 
 function getProperty(obj, p){
-	return obj.hasOwnProperty(p) ? obj[p] : '';
+  return obj.hasOwnProperty(p) ? obj[p] : '';
 }
 
 function completeUrl(url, key){
-	if (url == null || url == '' || key == null || key == '')
-	  return '';
-	uri = new Uri(url).setProtocol('').setHost('').setPort('')
-	  .deleteQueryParam('timeStamp', 'hash').addQueryParam('timeStamp', 
-	    new Date().getTime().toString());
-	uri.setQuery(sortProperties(uri.getQueryParams()));
+  if (url == null || url == '' || key == null || key == '')
+    return '';
+  uri = new Uri(url).setProtocol('').setHost('').setPort('')
+    .deleteQueryParam('timeStamp', 'hash').addQueryParam('timeStamp', 
+      new Date().getTime().toString());
+  uri.setQuery(sortProperties(uri.getQueryParams()));
   hash = new Hashes.SHA256().hex_hmac(uri.toString(), key);
   uri = uri.addQueryParam('hash', hash);
   return uri.toString();
